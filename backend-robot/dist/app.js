b4c9b80d5b1b5fcebb4dfec214378cc8998e248e
Robo Breet <devilrex429@gmail.com>
2026-01-12 17:22:44 +0500

    Add GitHub Actions CI/CD

--- backend-robot/dist/app.js
+++ backend-robot/dist/app.js
@@ -9,6 +9,7 @@ const allowedOrigins = [
     'http://localhost:5174',
     'http://localhost:5175',
     'http://localhost:8080',
+    'http://localhost:3000',
     'https://imagetools.shop',
     'http://imagetools.shop'
 ];
