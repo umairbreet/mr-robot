af40ce9ce09a6457a1dc08a3f9a2501f0aa67b47
Robo Breet <devilrex429@gmail.com>
2026-01-12 12:29:47 +0000

    Deploy: Build 2026-01-12 12:29:47

---
+++ backend-robot/dist/routes/contentCardRoutes.js
@@ -0,0 +1,6 @@
+import { Router } from 'express';
+import { createCard, fetchCards } from '../controllers/contentCardControllers.js';
+const router = Router();
+router.get('/cards', fetchCards); // Fetch all cards
+router.post('/cards', createCard); // Create a new card
+export default router;
